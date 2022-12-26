58f5a40494a2bd340b7c05b3d7d99ef9b3390d3d
skynettw <skynet.tw@gmail.com>
2022-12-26 21:26:24 +0800

    ch06 finish

--- dj4ch06/dj4ch06/urls.py
+++ dj4ch06/dj4ch06/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('', views.index),
     path('engtv/', views.engtv),
     path('engtv/<int:tvno>/', views.engtv, name='engtv-url'),
+    path('carlist/', views.carlist),
+    path('carlist/<int:maker>/', views.carlist, name='carlist-url'),
+    path('carprice/', views.carprice),
+    path('carprice/<int:maker>/', views.carprice, name='carprice-url'),
     path('<int:tvno>/', views.index, name = 'tv-url')
 ]
--- dj4ch06/mysite/views.py
+++ dj4ch06/mysite/views.py
@@ -7,6 +7,7 @@ def index(request, tvno = 0):
         {'name':'民視', 'tvcode':'ylYJSBUgaMA'},
         {'name':'中視', 'tvcode':'TCnaIE_SAtM'},]
     now = datetime.now()
+    hour = now.timetuple().tm_hour
     tv = tv_list[tvno]
     return render(request, 'index.html', locals())
 
@@ -20,4 +21,35 @@ def engtv(request, tvno='0'):
     tvno = tvno
     tv = tv_list[int(tvno)]
     return render(request, 'engtv.html', locals())
+
+def carlist(request, maker=0):
+    car_maker = ['SAAB', 'Ford', 'Honda', 'Mazda', 'Nissan','Toyota' ]
+    car_list = [ [],
+            ['Fiesta', 'Focus', 'Modeo', 'EcoSport', 'Kuga', 'Mustang'],
+            ['Fit', 'Odyssey', 'CR-V', 'City', 'NSX'],
+            ['Mazda3', 'Mazda5', 'Mazda6', 'CX-3', 'CX-5', 'MX-5'],
+            ['Tida', 'March', 'Livina', 'Sentra', 'Teana', 'X-Trail', 'Juke', 'Murano'],
+            ['Camry','Altis','Yaris','86','Prius','Vios', 'RAV4', 'Wish']
+              ]
+    maker = maker
+    maker_name =  car_maker[maker]
+    cars = car_list[maker]
+    return render(request, 'carlist.html', locals())
+    
+def carprice(request, maker=0):
+    car_maker = ['Ford', 'Honda', 'Mazda']
+    car_list = [[   {'model':'Fiesta', 'price': 203500}, 
+                    {'model':'Focus','price': 605000}, 
+                    {'model':'Mustang','price': 900000}],
+                [   {'model':'Fit', 'price': 450000}, 
+                    {'model':'City', 'price': 150000}, 
+                    {'model':'NSX', 'price':1200000}],
+                [   {'model':'Mazda3', 'price': 329999}, 
+                    {'model':'Mazda5', 'price': 603000},
+                    {'model':'Mazda6', 'price':850000}],
+              ]
+    maker = maker
+    maker_name =  car_maker[maker]
+    cars = car_list[maker]
+    return render(request, 'carprice.html', locals())
 
