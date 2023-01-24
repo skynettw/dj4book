b9b4157b43d2fc2669707bb229af73e8b4fcbae4
Richard <skynet.tw@gmail.com>
2023-01-24 14:15:28 +0800

    ch07 completed

--- dj4ch07/dj4ch07/dj4ch07/settings.py
+++ dj4ch07/dj4ch07/dj4ch07/settings.py
@@ -74,10 +74,23 @@ WSGI_APPLICATION = 'dj4ch07.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ch07www',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
