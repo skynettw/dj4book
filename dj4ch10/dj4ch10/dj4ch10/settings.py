5aa92137e20aef315efeed0684205f1d1ade27b0
Richard <skynet.tw@gmail.com>
2023-02-02 16:49:41 +0800

    mshop project finished

--- dj4ch10/dj4ch10/dj4ch10/settings.py
+++ dj4ch10/dj4ch10/dj4ch10/settings.py
@@ -129,8 +129,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "p179457@gmail.com"
-EMAIL_HOST_PASSWORD = "suximywysrzvuvzi"
+EMAIL_HOST_USER = "youremail@gmail.com"
+EMAIL_HOST_PASSWORD = "yourpassword"
 
 ACCOUNT_ACTIVATION_DAYS = 7
 
