90826b85c4734e77964e60ff89564e05104b43fb
stocklin <a6898208@gmail.com>
2023-05-20 20:38:32 +0800

    add chapter 13.

---
+++ dj4ch13/shop/urls.py
@@ -0,0 +1,37 @@
+"""shop URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.apps import apps
+from django.urls import include, path
+from django.contrib import admin
+
+
+urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
+
+    # The Django admin is not officially supported; expect breakage.
+    # Nonetheless, it's often useful for debugging.
+
+    path('admin/', admin.site.urls),
+    path('checkout/paypal/', include('paypal.express.urls')),
+    path('', include(apps.get_app_config('oscar').urls[0])),
+]
+
+if settings.DEBUG:
+    # Server statics and uploaded media
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
